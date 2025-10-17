2cca374a2799c78499a33bd16708d47756fa61e4
wilford-schaffner <wilford.schaffner@gmail.com>
2025-10-17 18:29:33 -0500

    welcome message on "/" dir

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const recipesRoutes = require('./recipes');
 
+// Root route - welcome message
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Recipe Manager API!',
+    version: '1.0.0',
+    endpoints: {
+      recipes: '/recipes',
+      documentation: '/api-docs'
+    },
+    usage: 'Visit /api-docs for interactive API documentation'
+  });
+});
+
 // Mount recipe routes at /recipes
 router.use('/recipes', recipesRoutes);
 
